168452e38ab59f0a95120acbe971c4914b2ed448
Ajay Singh <ajay.singh09795@gmail.com>
2017-11-21 16:05:59 +0530

    Add files via upload
    There is a ladder. On the 3rd rung of the ladder is a monkey, the 5th rung has a squirrel, the 8th rung has a pigeon, the 15th rung has an eagle, the 17th rung has another monkey.
    
    The monkey has 2 eyes, 2 hands, 2 legs. The squirrel has 2 eyes, 4 legs. The pigeon has 2 eyes, 2 wings, 2 legs and can fly. The eagle has 2 eyes, 2 wings, 2 legs and can fly.

---
+++ ladder-user.py
@@ -0,0 +1,18 @@
+from ladderutils import ladder
+
+l = ladder()
+print (l.animal_at_rung(3))
+print (l.animal_at_rung(5))
+print (l.animal_at_rung(8))
+print (l.animal_at_rung(15))
+print (l.animal_at_rung(10))
+print (l.get_animals_count())
+print (l.animal_at_rung(3) == l.animal_at_rung(17))
+print (type(l.animal_at_rung(3)) == type(l.animal_at_rung(17)))
+print (l.animal_at_rung(8).fly())
+print (l.animal_at_rung(3).fly())
+print (l.hop(3))
+print (l.animal_at_rung(3))
+print (l.animal_at_rung(4))
+print (l.hop(4))
+print (l.animal_at_rung(4))
